ba8a58f09b50e210ea60b906b89a0202f6f9ff9e
Ian <ianpang126@gmail.com>
2026-02-07 17:45:36 -0500

    serverless

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # Add the project root to PYTHONPATH so we can import 'backend'
-root_path = Path(__file__).resolve().parent.parent.parent
+root_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_path))
 
 from backend.main import app
